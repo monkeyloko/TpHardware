ef87a1f5cd681d501fcb5d6969e735d1fc7621a5
monkeyloko <defesanti@gmail.com>
2023-11-02 23:34:45 -0300

    avance

--- Hardware/src/components/Temperatura.js
+++ Hardware/src/components/Temperatura.js
@@ -1,40 +1,86 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import axios from 'axios';
 import * as Location from 'expo-location';
 
-const Temperatura=()=> {
+const Temperatura = () => {
+  const [date, setDate] = useState(new Date());
+  const [temperature, setTemperature] = useState(null);
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
-  const [weather, setWeather] = useState({})
 
   useEffect(() => {
     (async () => {
+      // Obtener la ubicación
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
-        return;
+      } else {
+        let userLocation = await Location.getCurrentPositionAsync({});
+        setLocation(userLocation);
+
+        // Obtener la temperatura actual
+        try {
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${userLocation.coords.latitude}&lon=${userLocation.coords.longitude}&APPID=0cd4c845628a93ee3dd46acea3646046&units=metric`
+          );
+          setTemperature(response.data.main.temp);
+        } catch (error) {
+          setErrorMsg('Error fetching temperature data');
+        }
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);    
-      const weather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&APPID=0cd4c845628a93ee3dd46acea3646046&units=metric`)
-
     })();
+
+    // Actualizar la fecha y hora cada segundo
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
   return (
-    <View>
-      <Text>{text}</Text>
+    <View style={styles.container}>
+      <Text style={styles.dateTime}>{date.toLocaleString()}</Text>
+      {temperature !== null ? (
+        <Text style={styles.temperature}>Temperatura: {temperature}°C</Text>
+      ) : (
+        <Text style={styles.loading}>Cargando temperatura...</Text>
+      )}
+
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dateTime: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  temperature: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  loading: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  location: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  error: {
+    fontSize: 16,
+    color: 'red',
+    marginBottom: 10,
+  },
+});
 
 export default Temperatura;
