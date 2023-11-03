fad38cef5cfd4921f39f528c1d94220a11637948
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-03 11:58:26 -0300

    avance

--- Hardware/src/components/QR.js
+++ Hardware/src/components/QR.js
@@ -1,40 +1,73 @@
 import QRCode from "react-native-qrcode-svg";
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+
 const QR = () => {
-    const [hasPermission, setHasPermission] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [qrPressed, setQrPressed] = useState(false);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     };
 
     getBarCodeScannerPermissions();
   }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
+  const handleBarCodeScanned = ({ data }) => {
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alert(`El trabajo fue realizado por ${data}`);
   };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  let foto = require("../zheoden/22899701.jfif");
+
   return (
-    <>
-      <QRCode
-        value="Joaquin Maceira, Santiago Doff, Lorenzo Shammah Zalba"
-        logo={foto}
-        logoSize={30}
-      />
-    </>
+    <View style={styles.container}>
+      <View style={styles.qrContainer}>
+        <QRCode
+          value="Joaquin Maceira, Santiago Doff, Lorenzo Shammah Zalba"
+          size={350}
+        />
+      </View>
+      {!hasPermission ? (
+        <Text>Requesting for camera permission</Text>
+      ) : (
+        <View style={styles.cameraContainer}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+          {scanned && (
+            <Button
+              title={"Tap to Scan Again"}
+              onPress={() => setScanned(false)}
+            />
+          )}
+        </View>
+      )}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  qrContainer: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+    marginBottom: 20,
+  },
+  cameraContainer: {
+    flex: 1,
+    width: "100%",
+  },
+});
+
 export default QR;
